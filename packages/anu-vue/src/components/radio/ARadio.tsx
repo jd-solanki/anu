95c2bb2983be1277c0695ab5d6cfaa6ff86d9717
JD Solanki <jdsolanki0001@gmail.com>
2022-06-18 15:21:27 +0530

    feat(radio): disabled style updated

--- packages/anu-vue/src/components/radio/ARadio.tsx
+++ packages/anu-vue/src/components/radio/ARadio.tsx
@@ -33,7 +33,7 @@ export const ARadio = defineComponent({
             <div class={[
                 `after:bg-${props.color}`,
                 isChecked.value ? `after:scale-full border-${props.color}` : 'after:scale-0',
-                `${isEleDisabled(attrs) ? 'border-dotted opacity-50' : 'border-solid'}`,
+                `${isEleDisabled(attrs) ? 'border-dashed' : 'border-solid'}`,
                 "h-5 w-5 border-(2 [hsl(var(--border-color))]) rounded-full mr-2 p-1 after:(w-full h-full rounded-full block content-empty transform transition transition-transform duration-250 ease-in-out)",
             ]} />
             {slots.default ? slots.default() : props.label}
