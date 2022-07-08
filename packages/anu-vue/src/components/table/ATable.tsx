773d16aa71162fae344a250712da6d9fc2e89732
JD Solanki <jdsolanki0001@gmail.com>
2022-07-08 23:55:40 +0530

    feat(table): sorting completed

--- packages/anu-vue/src/components/table/ATable.tsx
+++ packages/anu-vue/src/components/table/ATable.tsx
@@ -2,19 +2,23 @@ import { ACard, useCardProps } from '@/components/card'
 import { AInput } from '@/components/input'
 import type { CustomFilter } from '@/composables/useSearch'
 import { useSearch } from '@/composables/useSearch'
-import type { CustomSort } from '@/composables/useSort'
+import type { CustomSort, typeSortBy } from '@/composables/useSort'
 import { useSort } from '@/composables/useSort'
-import type { PropType } from 'vue'
+import type { PropType, Ref } from 'vue'
 import { computed, defineComponent, ref, toRef } from 'vue'
 
 // import { controlledComputed } from '@vueuse/core';
 
-interface Column {
+interface PropColumn {
   name: string
   isFilterable?: boolean
   filterBy?: CustomFilter
   isSortable?: boolean
   sortBy?: CustomSort
+}
+
+interface TableColumn extends PropColumn {
+  shallSortByAsc: boolean | null
 }
 
 export const ATable = defineComponent({
@@ -26,7 +30,7 @@ export const ATable = defineComponent({
       required: true,
     },
     columns: {
-      type: [Array] as PropType<Column[]>,
+      type: [Array] as PropType<PropColumn[]>,
       default: () => [],
     },
     search: {
@@ -52,10 +56,11 @@ export const ATable = defineComponent({
     const columnDefaults = {
       isFilterable: true,
       isSortable: true,
+      shallSortByAsc: null,
     }
 
     // If columns are provided via prop
-    const _columns: Column[] = props.columns.length
+    const _columns: Ref<TableColumn[]> = computed(() => props.columns.length
 
       // Inject column defaults by iterating over each col
       ? props.columns.map(c => ({ ...columnDefaults, ...c }))
@@ -67,11 +72,11 @@ export const ATable = defineComponent({
               ...columnDefaults,
               name: k,
             }))
-          : [])
+          : []))
 
     // Filter out columns that is searchable based on isFilterable property
-    const searchableCols = _columns.filter(col => col.isFilterable || col.filterBy)
-    const sortedCols = ref<Column[]>([])
+    const searchableCols = _columns.value.filter(col => col.isFilterable || col.filterBy)
+    const sortedCols = ref <TableColumn[]>([])
 
     const { results: filteredRows } = useSearch(
       _search,
@@ -84,28 +89,102 @@ export const ATable = defineComponent({
 
     const { results: sortedRows } = useSort(
       filteredRows,
-      computed(() => sortedCols.value.map(col => col.sortBy ? { name: col.name, sortBy: col.sortBy } : col.name)),
+      computed(() => {
+        const colsSortBy: typeSortBy = []
+
+        sortedCols.value.forEach(col => {
+          if (col.sortBy)
+            colsSortBy.push({ name: col.name, sortBy: col.sortBy })
+          else if (col.shallSortByAsc !== null)
+            colsSortBy.push({ name: col.name, isAsc: col.shallSortByAsc })
+        })
+
+        return colsSortBy
+      }),
     )
 
-    const handleHeaderClick = (col: Column) => {
-      if (!props.multiSort) {
-        sortedCols.value = [col]
+    const handleHeaderClick = (col: TableColumn) => {
+      // console.log('col :>> ', col)
 
-        return
-      }
-
-      // If multiSort is enabled
       const index = sortedCols.value.indexOf(col)
 
-      if (index > -1)
-        sortedCols.value.splice(index, 1)
-      else sortedCols.value.push(col)
+      // console.log('index :>> ', index)
+
+      /*
+        If col exist in arr
+          if sort order is asc
+            set sort order to desc
+          else
+            set sort order to null
+            and remove the column from `sortedCols`
+        else
+          set sort order to asc
+
+        // multi-sort
+
+        if sort order is not null
+          if multi-sort is not enabled
+            assign above modified col as single element of array to `sortedCols`
+          else
+            add modified col to sorted cols
+      */
+
+      if (index > -1) {
+        // Sorted by Asc
+        if (col.shallSortByAsc) {
+          col.shallSortByAsc = false
+        }
+
+        // Sorted by Desc
+        else {
+          col.shallSortByAsc = null
+
+          // console.log('in')
+          // console.log('sortedCols.value :>> ', sortedCols.value)
+          sortedCols.value.splice(index, 1)
+
+          // console.log('sortedCols.value :>> ', sortedCols.value)
+          // console.log('out')
+        }
+      }
+
+      // Not sorted
+      else {
+        col.shallSortByAsc = true
+      }
+
+      // console.log('col :>> ', col)
+
+      // Handle Multi sort
+
+      if (col.shallSortByAsc !== null) {
+        if (!props.multiSort)
+          sortedCols.value = [col]
+        else
+          sortedCols.value.push(col)
+      }
+
+      // console.log('col :>> ', col)
+      // console.log('sortedCols :>> ', sortedCols.value)
+      // console.log('-----')
+
+      // -------------
+
+      // if (!props.multiSort) {
+      //   sortedCols.value = [col]
+
+      //   return
+      // }
+
+      // if (index > -1)
+      //   sortedCols.value.splice(index, 1)
+      // else sortedCols.value.push(col)
     }
 
     return () => {
       // 👉 No results
       const noResultsTr = <tr>
-        <td colspan={_columns.length} class="px-4 h-12 whitespace-nowrap text-center font-medium">
+        <td colspan={_columns.value.length} class="px-4 h-12 whitespace-nowrap text-center font-medium">
           {slots.noResults ? slots.noResults() : <span>{props.noResultsText}</span>}
         </td>
       </tr>
@@ -122,7 +201,18 @@ export const ATable = defineComponent({
         {/* 👉 thead */}
         <thead>
           <tr>
-            {_columns.map(column => <th class="px-4 h-12 text-left whitespace-nowrap cursor-pointer" onClick={() => handleHeaderClick(column)}>{column.name}</th>)}
+            {_columns.value.map(column =>
+              <th
+                class="select-none px-4 h-12 text-left whitespace-nowrap cursor-pointer"
+                onClick={() => handleHeaderClick(column)}
+              >
+                <div class="inline-flex items-center">
+                    <span>{column.name}</span>
+                    <div v-show={column.shallSortByAsc === true} class="i-bx-up-arrow-alt"></div>
+                    <div v-show={column.shallSortByAsc === false} class="i-bx-down-arrow-alt"></div>
+                  </div>
+              </th>,
+            )}
           </tr>
         </thead>
 
@@ -144,13 +234,15 @@ export const ATable = defineComponent({
 
       const searchInput = () => <AInput prepend-inner-icon="i-bx-search" placeholder="search..." class="max-w-48" v-model={_search.value}></AInput>
 
+      const temp = <pre>{JSON.stringify(_columns.value, null, 2)}</pre>
+
       // 💡 Here we are passing all the slots to card except default which gets overridden for merging provided default slot with table
       return <ACard
         {...cardProps}
         class="overflow-auto"
         v-slots={{
           ...slots,
-          default: () => [slots.default?.(), table],
+          default: () => [slots.default?.(), temp, table],
           headerRight: typeof props.search === 'boolean' && props.search ? searchInput : null,
         }}
       />
