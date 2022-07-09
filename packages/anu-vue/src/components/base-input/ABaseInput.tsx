96556a51e0645a802f43167783224d017910728d
JD Solanki <jdsolanki0001@gmail.com>
2022-07-09 18:30:06 +0530

    feat(base-input): bottom content is transitioned

--- packages/anu-vue/src/components/base-input/ABaseInput.tsx
+++ packages/anu-vue/src/components/base-input/ABaseInput.tsx
@@ -1,90 +1,91 @@
-import { isEleDisabled, isEleInteractive } from '@/utils/dom';
-import { defineComponent, PropType, ref } from 'vue';
+import TransitionExpand from '@/transitions/TransitionExpand.vue'
+import { isEleDisabled, isEleInteractive } from '@/utils/dom'
+import type { PropType } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export const ABaseInput = defineComponent({
-    inheritAttrs: false,
-    name: 'ABaseInput',
-    inheritAttrs: false,
-    props: {
-        inputWrapperClasses: [Array, String, Object] as PropType<string | string[] | object>,
-        inputContainerAttrs: Object,
-        hint: String,
-        error: String,
-        label: String,
-        prependIcon: String,
-        appendIcon: String,
-        prependInnerIcon: String,
-        appendInnerIcon: String,
-    },
-    setup(props, { slots, attrs, expose }) {
-        const iconTransition = "transition duration-150 ease -in"
-        const elementId = attrs.id || props.label ? `a-input-${attrs.id || props.label}` : undefined
+  name: 'ABaseInput',
+  inheritAttrs: false,
+  props: {
+    inputWrapperClasses: [Array, String, Object] as PropType<string | string[] | object>,
+    inputContainerAttrs: Object,
+    hint: String,
+    error: String,
+    label: String,
+    prependIcon: String,
+    appendIcon: String,
+    prependInnerIcon: String,
+    appendInnerIcon: String,
+  },
+  setup(props, { slots, attrs, expose }) {
+    const iconTransition = 'transition duration-150 ease -in'
+    const elementId = attrs.id || props.label ? `a-input-${attrs.id || props.label}` : undefined
 
-        const refRoot = ref()
-        const refInputContainer = ref()
-        expose({
-            refRoot: refRoot,
-            refInputContainer: refInputContainer,
-        })
+    const refRoot = ref()
+    const refInputContainer = ref()
+    expose({
+      refRoot,
+      refInputContainer,
+    })
 
-        const { class: rootClasses, ...inputAttrs } = attrs
+    const { class: rootClasses, ...inputAttrs } = attrs
 
-        return () => <div class={["i:children:focus-within:text-primary flex flex-col flex-grow flex-shrink-0 gap-y-1", rootClasses ?? []]} ref={refRoot}>
+    return () => <div class={['i:children:focus-within:text-primary flex flex-col flex-grow flex-shrink-0 gap-y-1', rootClasses ?? []]} ref={refRoot}>
             {/* 👉 Label */}
             {
                 slots.label
-                    ? slots.label?.()
-                    : props.label
-                        ? <label for={elementId} class={['a-base-input-label', props.error ? 'text-danger' : null]}>{props.label}</label>
-                        : null
+                  ? slots.label?.()
+                  : props.label
+                    ? <label for={elementId} class={['a-base-input-label', props.error ? 'text-danger' : null]}>{props.label}</label>
+                    : null
             }
 
-            <div ref={refInputContainer} class="flex i:flex-shrink-0 i:w-6 i:h-6 gap-x-3 items-center" {...props.inputContainerAttrs}>
+            <div ref={refInputContainer} class="flex i:flex-shrink-0 i:em:w-6 i:em:h-6 gap-x-3 items-center" {...props.inputContainerAttrs}>
                 {/* 👉 Slot: Prepend */}
                 {
                     slots.prepend
-                        ? slots.prepend?.()
-                        : props.prependIcon
-                            ? <i class={[iconTransition, props.prependIcon]} />
-                            : null
+                      ? slots.prepend?.()
+                      : props.prependIcon
+                        ? <i class={[iconTransition, props.prependIcon]} />
+                        : null
                 }
 
                 {/* SECTION Input wrapper */}
                 <div class={[
                     `${props.error ? 'border-danger' : 'focus-within:border-primary'}`,
-                    'relative i:focus-within:text-primary transition duration-250 ease-out flex i:flex-shrink-0 i:w-5 i:h-5 gap-x-2 items-center min-h-12 border border-solid border-[hsla(var(--base-color),var(--border-opacity))] w-full rounded-lg',
+                    'relative i:focus-within:text-primary transition duration-250 ease-out flex i:flex-shrink-0 i:em:w-5 i:em:h-5 gap-x-2 items-center em:h-12 border border-solid border-[hsla(var(--base-color),var(--border-opacity))] w-full em:rounded-lg',
                     props.inputWrapperClasses,
                 ]}>
 
                     {/* 👉 Slot: Prepend Inner */}
                     {
                         slots['prepend-inner']
-                            ? slots['prepend-inner']?.()
-                            : props.prependInnerIcon
-                                ? <i class={['inline-block ml-3 z-1', iconTransition, props.prependInnerIcon]} />
-                                : null
+                          ? slots['prepend-inner']?.()
+                          : props.prependInnerIcon
+                            ? <i class={['inline-block ml-3 z-1', iconTransition, props.prependInnerIcon]} />
+                            : null
                     }
 
                     {/* 👉 Slot: Default */}
                     {slots.default?.({
-                        class: [
-                            "absolute inset-0 rounded-inherit",
-                            slots['prepend-inner'] || props.prependInnerIcon ? 'pl-10' : 'pl-4',
-                            slots['append-inner'] || props.appendInnerIcon ? 'pr-10' : 'pr-4',
+                      class: [
+                        'absolute inset-0 rounded-inherit',
+                        slots['prepend-inner'] || props.prependInnerIcon ? 'em:pl-10' : 'em:pl-4',
+                        slots['append-inner'] || props.appendInnerIcon ? 'em:pr-10' : 'em:pr-4',
                             `${isEleDisabled(attrs) ? 'bg-gray-200 opacity-50' : ''}`,
-                            `${isEleInteractive(attrs) ? 'placeholder:transition placeholder:duration-250 placeholder:ease  focus:placeholder:translate-x-1' : ''}`
-                        ],
-                        ...inputAttrs,
-                        id: elementId,
+                            `${isEleInteractive(attrs) ? 'placeholder:transition placeholder:duration-250 placeholder:ease  focus:placeholder:translate-x-1' : ''}`,
+                      ],
+                      ...inputAttrs,
+                      id: elementId,
                     })}
 
                     {/* 👉 Slot: Append Inner */}
                     {
                         slots['append-inner']
-                            ? slots['append-inner']?.()
-                            : props.appendInnerIcon
-                                ? <i class={['inline-block mr-3 ml-auto', iconTransition, props.appendInnerIcon]} />
-                                : null
+                          ? slots['append-inner']?.()
+                          : props.appendInnerIcon
+                            ? <i class={['inline-block em:me-3 ml-auto', iconTransition, props.appendInnerIcon]} />
+                            : null
                     }
                 </div>
                 {/* !SECTION */}
@@ -92,26 +93,24 @@ export const ABaseInput = defineComponent({
                 {/* 👉 Slot: Append */}
                 {
                     slots.append
-                        ? slots.append?.()
-                        : props.appendIcon
-                            ? <i class={[iconTransition, props.appendIcon]} />
-                            : null
+                      ? slots.append?.()
+                      : props.appendIcon
+                        ? <i class={[iconTransition, props.appendIcon]} />
+                        : null
                 }
             </div>
             {/* 👉 Slot: Bottom */}
             {
                 slots.bottom
-                    ? slots.bottom?.()
-                    : <div class="h-8">
-                        {
-                            props.error || props.hint
-                                ? <small class={`inline-block ${props.error ? 'text-danger' : 'text-gray-400'}`}>{props.error || props.hint}</small>
-                                : null
-                        }
+                  ? slots.bottom?.()
+                  : <TransitionExpand>
+                      <div class="h-8" v-show={props.error || props.hint}>
+                        <small class={`inline-block ${props.error ? 'text-danger' : 'text-gray-400'}`}>{props.error || props.hint}</small>
                     </div>
+                  </TransitionExpand>
             }
         </div>
-    },
+  },
 })
 
 export type ABaseInput = InstanceType<typeof ABaseInput>
---
+++ packages/anu-vue/src/transitions/TransitionExpand.vue
@@ -0,0 +1,89 @@
+<!-- Thanks: https://markus.oberlehner.net/blog/transition-to-height-auto-with-vue/ -->
+<script lang="ts">
+import { Transition, defineComponent, h } from 'vue'
+export default defineComponent({
+  name: 'TransitionExpand',
+  setup(_, { slots }) {
+    const onEnter = (element: HTMLElement) => {
+      const width = getComputedStyle(element).width
+
+      element.style.width = width
+      element.style.position = 'absolute'
+      element.style.visibility = 'hidden'
+      element.style.height = 'auto'
+
+      const height = getComputedStyle(element).height
+
+      element.style.width = ''
+      element.style.position = ''
+      element.style.visibility = ''
+      element.style.height = '0px'
+
+      // Force repaint to make sure the
+      // animation is triggered correctly.
+      // eslint-disable-next-line no-unused-expressions
+      getComputedStyle(element).height
+
+      // Trigger the animation.
+      // We use `requestAnimationFrame` because we need
+      // to make sure the browser has finished
+      // painting after setting the `height`
+      // to `0` in the line above.
+      requestAnimationFrame(() => {
+        element.style.height = height
+      })
+    }
+
+    const onAfterEnter = (element: HTMLElement) => {
+      element.style.height = 'auto'
+    }
+    const onLeave = (element: HTMLElement) => {
+      const height = getComputedStyle(element).height
+
+      element.style.height = height
+
+      // Force repaint to make sure the
+      // animation is triggered correctly.
+      // eslint-disable-next-line no-unused-expressions
+      getComputedStyle(element).height
+
+      requestAnimationFrame(() => {
+        element.style.height = '0px'
+      })
+    }
+
+    return () => h(
+      h(Transition),
+      {
+        name: 'expand',
+        onEnter,
+        onAfterEnter,
+        onLeave,
+      },
+      () => slots.default?.(),
+    )
+  },
+})
+</script>
+
+<style>
+.expand-enter-active,
+.expand-leave-active {
+  overflow: hidden;
+  transition: block-size var(--expand-transition-duration, 0.25s) ease;
+}
+
+.expand-enter-from,
+.expand-leave-to {
+  block-size: 0;
+}
+</style>
+
+<style scoped>
+* {
+  backface-visibility: hidden;
+  perspective: 1000px;
+  transform: translateZ(0);
+  will-change: block-size;
+}
+</style>
