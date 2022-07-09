6c82d885c0f4ffb9de9d2fee42bbd0bf3eba9a9b
JD Solanki <jdsolanki0001@gmail.com>
2022-07-09 18:31:42 +0530

    fix(typography): typography props are now reactive

--- packages/anu-vue/src/components/typography/ATypography.tsx
+++ packages/anu-vue/src/components/typography/ATypography.tsx
@@ -1,56 +1,77 @@
-import { useTypographyProps } from '@/composables/useTypography';
-import { defineComponent } from 'vue';
+import { useTypographyProps } from '@/composables/useTypography'
+import { computed, defineComponent } from 'vue'
 
 export const ATypography = defineComponent({
-    name: 'ATypography',
-    props: {
-        ...useTypographyProps(),
-    },
-    setup(props, { slots }) {
+  name: 'ATypography',
+  props: {
+    ...useTypographyProps(),
+  },
+  setup(props, { slots }) {
+    const title = computed(() => {
+      const [titleContent, titleClasses] = props.title === undefined
+        ? []
+        : typeof props.title === 'string'
+          ? [props.title]
+          : props.title
 
-        const [titleContent, titleClasses] = props.title == undefined
-            ? []
-            : typeof props.title === 'string'
-                ? [props.title]
-                : props.title
+      return {
+        titleContent,
+        titleClasses,
+      }
+    })
 
-        const [subtitleContent, subtitleClasses] = props.subtitle == undefined
-            ? []
-            : typeof props.subtitle === 'string'
-                ? [props.subtitle]
-                : props.subtitle
+    // const [subtitleContent, subtitleClasses] = computed(() => props.subtitle === undefined
+    const subtitle = computed(() => {
+      const [subtitleContent, subtitleClasses] = props.subtitle === undefined
+        ? []
+        : typeof props.subtitle === 'string'
+          ? [props.subtitle]
+          : props.subtitle
 
-        const [textContent, textClasses] = props.text == undefined
-            ? []
-            : typeof props.text === 'string'
-                ? [props.text]
-                : props.text
+      return {
+        subtitleContent,
+        subtitleClasses,
+      }
+    })
 
-        // TODO: Remove class block and use commented tag defaults instead of span once VitePress allow style isolation
-        return () => <div class="uno-layer-base-text-base gap-4 flex flex-col">
+    const text = computed(() => {
+      const [textContent, textClasses] = props.text === undefined
+        ? []
+        : typeof props.text === 'string'
+          ? [props.text]
+          : props.text
+
+      return {
+        textContent,
+        textClasses,
+      }
+    })
+
+    // TODO: Remove class block and use commented tag defaults instead of span once VitePress allow style isolation
+    return () => <div class="uno-layer-base-text-base gap-4 flex flex-col">
             <div class="flex justify-between">
                 {/* Only render this header div if required */}
                 <div class="flex-grow">
                     {
                         slots.title || props.title
-                            ? <props.titleTag class={["block uno-layer-base-text-rlg uno-layer-base-text-[hsla(var(--typography-title-color),var(--typography-title-opacity))]", titleClasses]}>{slots.title ? slots.title() : titleContent}</props.titleTag>
-                            : null
+                          ? <props.titleTag class={['block uno-layer-base-text-rlg uno-layer-base-text-[hsla(var(--typography-title-color),var(--typography-title-opacity))]', title.value.titleClasses]}>{slots.title ? slots.title() : title.value.titleContent}</props.titleTag>
+                          : null
                     }
                     {
                         slots.subtitle || props.subtitle
-                            ? <props.subtitleTag class={["block uno-layer-base-text-rsm uno-layer-base-text-[hsla(var(--typography-subtitle-color),var(--typography-subtitle-opacity))]", subtitleClasses]}>{slots.subtitle ? slots.subtitle() : subtitleContent}</props.subtitleTag>
-                            : null
+                          ? <props.subtitleTag class={['block em:uno-layer-base-text-sm uno-layer-base-text-[hsla(var(--typography-subtitle-color),var(--typography-subtitle-opacity))]', subtitle.value.subtitleClasses]}>{slots.subtitle ? slots.subtitle() : subtitle.value.subtitleContent}</props.subtitleTag>
+                          : null
                     }
                 </div>
                 {slots.headerRight?.()}
             </div>
             {
                 slots.default || props.text
-                    ? <props.textTag class={["uno-layer-base-text-rbase uno-layer-base-text-[hsla(var(--typography-text-color),var(--typography-text-opacity))]", textClasses]}>{slots.default ? slots.default() : textContent}</props.textTag>
-                    : null
+                  ? <props.textTag class={['em:uno-layer-base-text-base uno-layer-base-text-[hsla(var(--typography-text-color),var(--typography-text-opacity))]', text.value.textClasses]}>{slots.default ? slots.default() : text.value.textContent}</props.textTag>
+                  : null
             }
         </div>
-    },
+  },
 })
 
 export type ATypography = InstanceType<typeof ATypography>
