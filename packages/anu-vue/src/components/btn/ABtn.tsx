23a5735aa0fba1aca289f53a0d4c0e32f3cc2243
JD Solanki <jdsolanki0001@gmail.com>
2022-07-09 18:30:40 +0530

    feat(btn): support disabled attr

--- packages/anu-vue/src/components/btn/ABtn.tsx
+++ packages/anu-vue/src/components/btn/ABtn.tsx
@@ -1,4 +1,5 @@
 import { useLayer, useProps as useLayerProps } from '@/composables/useLayer'
+import { isEleDisabled } from '@/utils/dom'
 import { defineComponent } from 'vue'
 
 export const ABtn = defineComponent({
@@ -24,13 +25,13 @@ export const ABtn = defineComponent({
     iconOnly: {
       type: Boolean,
       default: false,
-    }
+    },
   },
-  setup(props, { slots }) {
+  setup(props, { slots, attrs }) {
     const { getLayerClasses } = useLayer()
 
     // FIX: ABtn gets full width if placed inside flex container
-    return () => <button class={[props.iconOnly ? 'btn-icon-only' : 'btn', 'uno-layer-base-text-base inline-flex justify-center items-center', ...getLayerClasses(props)]}>
+    return () => <button class={[props.iconOnly ? 'btn-icon-only' : 'btn', 'uno-layer-base-text-base inline-flex justify-center items-center', { 'opacity-50 pointer-events-none': isEleDisabled(attrs) }, ...getLayerClasses(props)]}>
       {props.icon ? <i class={props.icon}></i> : null}{slots.default?.()}{props.appendIcon ? <i class={props.appendIcon}></i> : null}
     </button>
   },
