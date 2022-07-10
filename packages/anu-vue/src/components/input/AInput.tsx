df3bca30b6bef3cd34407b5f07a65d55cb5c1c08
JD Solanki <jdsolanki0001@gmail.com>
2022-07-10 22:19:08 +0530

    fix(input): input type file styling fixed for dark mode

--- packages/anu-vue/src/components/input/AInput.tsx
+++ packages/anu-vue/src/components/input/AInput.tsx
@@ -1,34 +1,34 @@
-import { ABaseInput } from '@/components/base-input';
-import { defineComponent } from 'vue';
+import { ABaseInput } from '@/components/base-input'
+import { defineComponent } from 'vue'
 
 export const AInput = defineComponent({
-    name: 'AInput',
-    props: {
-        modelValue: String,
-    },
-    emits: ['input', 'update:modelValue'],
-    setup(props, { slots, emit, attrs }) {
-        const isInputTypeFile = attrs.type && attrs.type === 'file'
+  name: 'AInput',
+  props: {
+    modelValue: String,
+  },
+  emits: ['input', 'update:modelValue'],
+  setup(props, { slots, emit, attrs }) {
+    const isInputTypeFile = attrs.type && attrs.type === 'file'
 
-        const handleChange = (e: InputEvent) => {
-            const val = (e.target as HTMLInputElement).value
-            emit('input', val)
-            emit('update:modelValue', val)
-        }
+    const handleChange = (e: InputEvent) => {
+      const val = (e.target as HTMLInputElement).value
+      emit('input', val)
+      emit('update:modelValue', val)
+    }
 
-        return () => <ABaseInput {...attrs}>
+    return () => <ABaseInput {...attrs}>
             {{
-                // Recursively pass down slots
-                ...slots,
-                default: (slotProps: any) => <input
+              // Recursively pass down slots
+              ...slots,
+              default: (slotProps: any) => <input
                     {...slotProps}
                     value={props.modelValue}
-                    class={isInputTypeFile ? "file:rounded-lg file:border-none file:mr-4 file:px-4 file:py-3 file:text-gray-500 file:rounded-r-none file:bg-gray-100 !px-0" : null}
+                    class={isInputTypeFile ? 'file:rounded-lg file:border-none file:mr-4 file:px-4 file:py-3 file:text-gray-500 file:rounded-r-none file:bg-[hsla(var(--base-color),0.05)] !px-0' : null}
                     onInput={handleChange}
-                />
+                />,
             }}
         </ABaseInput >
-    },
+  },
 })
 
 export type AInput = InstanceType<typeof AInput>
