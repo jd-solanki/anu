3649c2ca94282e88f484e1a7adbecdf0d02c2051
JD Solanki <jdsolanki0001@gmail.com>
2022-07-11 08:58:15 +0530

    fix(dialog): Dialog backdrop color in dark mode

--- packages/anu-vue/src/components/dialog/ADialog.tsx
+++ packages/anu-vue/src/components/dialog/ADialog.tsx
@@ -1,7 +1,7 @@
-import { ACard, useCardProps } from '@/components/card';
-import { useDOMScrollLock } from '@/composables/useDOMScrollLock';
-import { onClickOutside } from '@vueuse/core';
-import { defineComponent, ref, Teleport, toRef } from 'vue';
+import { ACard, useCardProps } from '@/components/card'
+import { useDOMScrollLock } from '@/composables/useDOMScrollLock'
+import { onClickOutside } from '@vueuse/core'
+import { defineComponent, ref, Teleport, toRef } from 'vue'
 
 export const ADialog = defineComponent({
   name: 'ADialog',
@@ -14,27 +14,25 @@ export const ADialog = defineComponent({
     persistent: {
       type: Boolean,
       default: false,
-    }
+    },
   },
   setup(props, { slots, emit, attrs }) {
-
-
     const refCard = ref()
     if (!props.persistent) {
       onClickOutside(refCard, () => {
         // If dialog is not open => Don't execute
-        if (!props.modelValue) return
+        if (!props.modelValue)
+          return
 
         emit('update:modelValue', false)
       })
-
     }
 
     // Lock DOM scroll when modelValue is `true`
     useDOMScrollLock(toRef(props, 'modelValue'))
 
     return () => <Teleport to="body">
-      <div {...attrs} class={[{ 'hidden': !props.modelValue }, "dialog grid uno-layer-base-place-items-center mx-auto z-[51] fixed uno-layer-base-inset-0 after:(content-empty fixed inset-0 bg-[hsla(var(--base-color),0.32)] z-[51])"]}>
+      <div {...attrs} class={[{ hidden: !props.modelValue }, 'dialog grid uno-layer-base-place-items-center mx-auto z-[51] fixed uno-layer-base-inset-0 after:(content-empty fixed inset-0 bg-[hsla(var(--base-color),0.22)] z-[51] backdrop-blur-[4px])']}>
         <ACard class="shadow-2xl uno-layer-base-w-[500px]" ref={refCard} class="z-[52]" {...props}>{{ ...slots }}</ACard>
       </div>
     </Teleport>
