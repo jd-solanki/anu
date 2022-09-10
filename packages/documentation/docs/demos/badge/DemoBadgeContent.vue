6bcf8be21ed56665c54f92d4bab198f05370c495
Daniel Apodaca <daniela@zoomcatalog.com>
2022-09-10 09:17:14 -0600

    docs(badge): fix demo prop

--- packages/documentation/docs/demos/badge/DemoBadgeContent.vue
+++ packages/documentation/docs/demos/badge/DemoBadgeContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-wrap gap-4">
-    <ABadge badge-content="9">
+    <ABadge :badge-content="9">
       <i class="inline-block i-bx-bell w-6 h-6" />
     </ABadge>
     <ABadge>
