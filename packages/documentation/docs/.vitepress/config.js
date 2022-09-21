07cba1434b686a7e6bd3557ee846ed4092d0fb82
JD Solanki <jdsolanki0001@gmail.com>
2022-09-21 09:53:17 +0530

    docs: reduced code required to render the demo card

--- packages/documentation/docs/.vitepress/config.js
+++ packages/documentation/docs/.vitepress/config.js
@@ -1,5 +1,9 @@
+import MarkdownIt from 'markdown-it'
+import Container from 'markdown-it-container'
 import Unocss from 'unocss/vite'
 import { defineConfig } from 'vitepress'
+
+md = new MarkdownIt()
 
 export default defineConfig({
   title: 'Anu',
@@ -67,6 +71,31 @@ export default defineConfig({
   },
   markdown: {
     theme: 'dracula',
+    config: md => {
+      md.use(Container, 'card', {
+        render: (tokens, idx) => {
+          const token = tokens[idx]
+
+          // console.log('token :>> ', token)
+
+          const title = token.info.trim().slice(5).trim()
+          const titleHtml = md.render(`## ${title}`)
+
+          return token.nesting === 1 ? `<Demo>${titleHtml}` : '</Demo>\n'
+        },
+      })
+
+      md.use(Container, 'code', {
+        render: (tokens, idx) => {
+          const token = tokens[idx]
+
+          // console.log('token :>> ', token)
+          const demoName = token.info.trim().slice(5).trim()
+
+          return token.nesting === 1 ? `<template #demo><${demoName} /></template><template #code>` : '</template>\n'
+        },
+      })
+    },
   },
   vite: {
     plugins: [
