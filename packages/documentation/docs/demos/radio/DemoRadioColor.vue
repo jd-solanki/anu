2cde774b8e11a0c5240a0166e8abeb81d5a936b7
noe <gnoe@foxmail.com>
2022-10-10 19:28:24 +0800

    chore: revert radio demo

--- packages/documentation/docs/demos/radio/DemoRadioColor.vue
+++ packages/documentation/docs/demos/radio/DemoRadioColor.vue
@@ -1,29 +1,16 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const favoriteFruit1 = ref()
-const favoriteFruit2 = ref()
+const favoriteFruit = ref()
 const colors = ['primary', 'success', 'info', 'warning', 'danger']
 </script>
 
 <template>
   <div class="flex flex-col gap-y-3 mt-6">
-    <h1>first group</h1>
     <ARadio
       v-for="color in colors"
       :key="color"
-      v-model="favoriteFruit1"
-      name="radio-color"
-      :color="color"
-      :value="color"
-      :label="color"
-      class="capitalize"
-    />
-    <h1>second group</h1>
-    <ARadio
-      v-for="color in colors"
-      :key="color"
-      v-model="favoriteFruit2"
+      v-model="favoriteFruit"
       name="radio-color"
       :color="color"
       :value="color"
