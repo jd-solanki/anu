a20aa010c9c5cea56b93cb4f72ef7831750e0da9
brojor <bronislav.jordan@gmail.com>
2022-10-31 09:18:30 +0100

    feat(ratig): add ability to halve icons

--- packages/anu-vue/src/components/rating/ARating.tsx
+++ packages/anu-vue/src/components/rating/ARating.tsx
@@ -15,15 +15,21 @@ export const ARating = defineComponent({
       default: 2.5,
       required: true,
     },
-
     itemsAmount: {
       type: Number,
       default: 5,
     },
-
+    halving: {
+      type: Boolean,
+      default: false,
+    },
     iconEmpty: {
       type: String,
       default: 'i-bx:star',
+    },
+    iconHalf: {
+      type: String,
+      default: 'i-bx:bxs-star-half',
     },
     iconFull: {
       type: String,
@@ -46,20 +52,33 @@ export const ARating = defineComponent({
       ref(false),
     )
 
+    const hoverValue = ref(0)
+
     const stars = computed(() =>
       Array.from({ length: props.itemsAmount }, (_, i) => i + 1).map(item =>
         item <= props.modelValue
           ? props.iconFull
-          : props.iconEmpty,
+          : item - props.modelValue === 0.5
+            ? props.iconHalf
+            : props.iconEmpty,
       ),
     )
 
     const handleStarClick = (index: number) => {
-      emit('update:modelValue', index + 1)
+      emit('update:modelValue', index + hoverValue.value)
+    }
+
+    const onMouseMove = (e: MouseEvent) => {
+      const { offsetX, target } = e
+      if (target instanceof HTMLElement) {
+        const starPercentage = (offsetX * 100) / target.clientWidth
+        props.halving ? (hoverValue.value = starPercentage < 50 ? 0.5 : 1) : (hoverValue.value = 1)
+      }
     }
 
     return () => (
-       <div class={['flex', ...classes.value]} style={[...styles.value]}>
+       <div class={['flex', ...classes.value]} style={[...styles.value]} onMousemove={onMouseMove}>
+
         {stars.value.map((icon, i) => {
           return <i class={[icon]} onClick={() => handleStarClick(i)}/>
         })}
