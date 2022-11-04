3ec2f4efee0b9e9401d8855dd2050fb6196fc0d9
JD Solanki <47495003+jd-solanki@users.noreply.github.com>
2022-11-04 11:07:25 +0530

    docs: updated discord invite link

--- packages/documentation/docs/.vitepress/config.js
+++ packages/documentation/docs/.vitepress/config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/jd-solanki/anu' },
-      { icon: 'discord', link: 'https://discord.gg/vSp3a2Yn' },
+      { icon: 'discord', link: 'https://discord.gg/8MTQuasmZf' },
     ],
     nav,
     sidebar: {
