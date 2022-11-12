933d9d7a468a8a59d2d00f22091dd9c41545daa7
JD Solanki <jdsolanki0001@gmail.com>
2022-11-12 20:16:42 +0530

    wip

--- packages/documentation/docs/demos/input/DemoInputBasic.vue
+++ packages/documentation/docs/demos/input/DemoInputBasic.vue
@@ -1,7 +1,20 @@
+<script lang="ts" setup>
+const log = () => {
+  console.log('me')
+}
+</script>
+
 <template>
   <div class="grid-row sm:grid-cols-2 place-items-stretch">
     <div>
-      <AInput />
+      <AInput class="w-12">
+        <template #append-inner>
+          <span
+            class="cursor-pointer"
+            @click.stop="log"
+          >%</span>
+        </template>
+      </AInput>
     </div>
   </div>
 </template>
--- packages/documentation/docs/demos/input/DemoInputStates.vue
+++ packages/documentation/docs/demos/input/DemoInputStates.vue
@@ -9,7 +9,13 @@ const name = ref('JD Solanki')
       v-model="name"
       disabled
       label="Disabled"
-    />
+    >
+      <template #prepend-inner>
+        <div class="bg-red bg-opacity-50 p-1 rounded-inherit text-sm text-red">
+          Stocks
+        </div>
+      </template>
+    </AInput>
     <AInput
       v-model="name"
       readonly
