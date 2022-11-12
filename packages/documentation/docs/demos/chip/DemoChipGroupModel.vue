179c4ee796d62b09b3c1adc7075aef59ef6da3d3
Bronislav Jordán <58697105+brojor@users.noreply.github.com>
2022-11-12 08:46:09 +0100

    docs(chip): Simplify groupModel demo
    Co-authored-by: JD Solanki <47495003+jd-solanki@users.noreply.github.com>

--- packages/documentation/docs/demos/chip/DemoChipGroupModel.vue
+++ packages/documentation/docs/demos/chip/DemoChipGroupModel.vue
@@ -1,28 +1,28 @@
 <script lang="ts" setup>
-/*eslint-disable */
 import { useGroupModel } from 'anu-vue'
 
 const { options, select, value } = useGroupModel({
   options: [
-    { title: 'User', icon: 'i-bx-user' },
-    { title: 'Charging', icon: 'i-bx-bxs-battery-charging' },
-    { title: 'Options', icon: 'i-bx-wrench' },
-    { title: 'Traffic', icon: 'i-bx-car' },
-    { title: 'Remove', icon: 'i-bx-trash' },
+    'VueJS',
+    'Pinia',
+    'Vue Router',
+    'Vitest',
+    'VueUse',
   ],
+  multi: true,
 })
 </script>
 
 <template>
-<div class="flex mb-4 gap-2">
-  <AChip
-    v-for="option in options"
-    :key="option.value.title"
-    :icon="option.value.icon"
-    @click="select(option.value)"
-    :variant="value === option.value ? 'fill' : 'light'"
-  >
-    {{ option.value.title }}
-  </AChip>
-</div>
+  <div class="flex mb-4 gap-2">
+    <AChip
+      v-for="option in options"
+      :key="option.value"
+      :variant="option.isSelected ? 'fill' : 'light'"
+      @click="select(option.value)"
+    >
+      {{ option.value }}
+    </AChip>
+  </div>
 </template>
+
