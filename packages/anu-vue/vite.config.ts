23e3251ea03dbb15fa042718891226339076264e
peterbudai@hotmail.com <peterbudai@hotmail.com>
2022-12-05 13:35:33 +0100

    build: Moving to es module only

--- packages/anu-vue/vite.config.ts
+++ packages/anu-vue/vite.config.ts
@@ -4,28 +4,33 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import pkg from './package.json'
+
+const externals = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+]
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
       name: 'anu-vue',
-      fileName: 'anu-vue',
+      fileName: format => `anu-vue.${format}.js`,
     },
     outDir: 'dist',
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: [
-        'vue',
-        '@floating-ui/dom',
-      ],
+      external: externals,
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue',
+          'vue': 'Vue',
+          '@floating-ui/dom': 'FloatingDom',
         },
       },
     },
