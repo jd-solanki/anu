c996604e9aff6c3d2ed0d1e1e480cff1a723aa85
JD Solanki <jdsolanki0001@gmail.com>
2022-12-17 17:56:46 +0530

    feat: avatar component refactored

---
+++ packages/anu-vue/src/components/avatar/AAvatar.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import { toRef } from 'vue'
+import type { AvatarOnlyProps } from './props'
+import type { LayerProps } from '@/composables/useLayer'
+import { useLayer } from '@/composables/useLayer'
+import { useSpacing } from '@/composables/useSpacing'
+
+interface Props extends AvatarOnlyProps, LayerProps {
+  spacing: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: 'primary',
+  variant: 'light',
+  alt: 'avatar',
+})
+
+defineOptions({
+  name: 'AAvatar',
+})
+
+const spacing = useSpacing(toRef(props, 'spacing'))
+const { getLayerClasses } = useLayer()
+const { styles, classes } = getLayerClasses(
+  toRef(props, 'color'),
+  toRef(props, 'variant'),
+  toRef(props, 'states'),
+)
+</script>
+
+<template>
+  <div
+    class="a-avatar overflow-hidden inline-flex items-center justify-center"
+    :class="classes"
+    :style="[...styles, { '--a-spacing': spacing / 100 }]"
+  >
+    <!-- Render default slot if it's being used -->
+    <slot v-if="$slots.default" />
+
+    <!-- Else, Render according to props provided: src, icon, content -->
+    <template v-else>
+      <img
+        v-if="props.src"
+        :src="props.src"
+        :alt="props.alt"
+      >
+      <i
+        v-else-if="props.icon"
+        :class="props.icon"
+      />
+      <span v-else>{{ props.content }}</span>
+    </template>
+  </div>
+</template>
--- packages/anu-vue/src/components/avatar/props.ts
+++ packages/anu-vue/src/components/avatar/props.ts
@@ -1,42 +1,8 @@
-import type { ExtractPropTypes, PropType } from 'vue'
 import type { ConfigurableValue } from '@/composables/useConfigurable'
 
-export const avatarOnlyProps = {
-  /**
-   * Icon to render in avatar
-   */
-  icon: {
-    type: [Array, String] as PropType<ConfigurableValue>,
-    required: false,
-    default: undefined,
-  },
-
-  /**
-   * Content to render inside avatar
-   */
-  content: {
-    type: String,
-    required: false,
-    default: undefined,
-  },
-
-  /**
-   * Render image using this prop
-   */
-  src: {
-    type: String,
-    required: false,
-    default: undefined,
-  },
-
-  /**
-   * Provide alt attribute for image given by `src` prop
-   */
-  alt: {
-    type: String,
-    required: false,
-    default: 'avatar',
-  },
+export interface AvatarOnlyProps {
+  icon?: ConfigurableValue
+  content?: string
+  src?: string
+  alt?: string
 }
-
-export type AvatarOnlyProps = ExtractPropTypes<typeof avatarOnlyProps>
