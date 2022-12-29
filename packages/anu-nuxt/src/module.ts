b513fcbe3c1c0faeabe5a472dcbe20d1030b1850
Christian Preston <christianpreston@ymail.com>
2022-12-28 23:08:28 -0500

    feat: add better nuxt 3 support with auto config

--- packages/anu-nuxt/src/module.ts
+++ packages/anu-nuxt/src/module.ts
@@ -1,4 +1,8 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
+import { presetAnu, presetIconExtraProperties } from 'anu-vue'
+import { presetThemeDefault } from '@anu-vue/preset-theme-default'
+import presetUno from '@unocss/preset-uno'
+import presetIcons from '@unocss/preset-icons'
 import {
   addComponent,
   addImportsSources,
@@ -9,21 +13,69 @@ import {
 
 import { name, version } from '../package.json'
 
-export interface ModuleOptions {}
+export interface ModuleOptions {
+
+  /**
+   * Import Anu Preset Theme Default | Source npm pkg: `@anu-vue/preset-theme-default`
+   * When enabled, it will automatically set up the default presets for Anu and Uno.
+   * We recommend to enable this option to get the best experience.
+   *
+   * @default true
+   */
+  presetTheme?: boolean
+}
 
 export default defineNuxtModule<ModuleOptions>({
+  defaults: {
+    presetTheme: true,
+  },
   meta: {
     name,
     version,
-    configKey: 'anuVue',
+    configKey: 'anu',
     compatibility: {
       nuxt: '^3.0.0',
     },
   },
-  setup(moduleOptions, nuxt) {
+  setup(opts, nuxt) {
     const { resolve } = createResolver(import.meta.url)
 
     nuxt.options.css.push('anu-vue/dist/style.css')
+
+    if (opts.presetTheme) {
+      // Import Anu Preset Theme Default CSS
+      nuxt.options.css.push('@anu-vue/preset-theme-default/dist/style.css')
+
+      const iconPreset = nuxt.options.unocss?.icons || true
+
+      nuxt.options.unocss = nuxt.options.unocss || {}
+      nuxt.options.unocss.preflight = false
+      nuxt.options.unocss.presets = [
+        // @ts-expect-error - We know that is a valid preset
+        presetUno(),
+        presetAnu(),
+
+        // @ts-expect-error - We know that is a valid preset
+        presetThemeDefault(),
+        ...(nuxt.options.unocss.presets || []),
+      ]
+
+      // If icon preset is enabled, add it to the presets
+      // Then if true we will add the default anu icon preset
+      if (iconPreset) {
+        nuxt.options.unocss.presets?.push(
+
+          // @ts-expect-error - We know that is a valid preset
+          presetIcons(typeof iconPreset === 'object'
+            ? iconPreset
+            : {
+                scale: 1.2,
+                extraProperties: presetIconExtraProperties,
+              },
+          ),
+        )
+      }
+    }
 
     nuxt.hook('prepare:types', ({ tsConfig, references }) => {
       tsConfig.compilerOptions!.types.push('anu-vue/volar')
@@ -95,7 +147,7 @@ declare module '@nuxt/schema' {
   interface ConfigSchema {
     runtimeConfig: {
       public?: {
-        anu?: ModuleOptions
+        anu?: {}
       }
     }
   }
