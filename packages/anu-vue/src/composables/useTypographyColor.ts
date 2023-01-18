86d0ce4cf33477306721252372e284d5466e2370
JD Solanki <jdsolanki0001@gmail.com>
2023-01-18 19:18:24 +0530

    wip

--- packages/anu-vue/src/composables/useTypographyColor.ts
+++ packages/anu-vue/src/composables/useTypographyColor.ts
@@ -3,16 +3,20 @@ import { resolveUnref } from '@vueuse/core'
 import type { ColorProp } from './useProps'
 import { isThemeColor } from '@/utils/color'
 
-const calculateColor = (_isThemeColor: boolean, _color: ColorProp | null) => {
+const calculateColor = (_isThemeColor: boolean, _color: ColorProp | null, _variant: string) => {
   const classes = []
   const styles = []
   if (_isThemeColor) {
-    classes.push(`a-title-${_color} a-subtitle-${_color} a-subtitle-opacity-100`)
+    classes.push('a-subtitle-opacity-100')
+    if (_variant !== 'fill')
+      classes.push(`a-title-${_color} a-subtitle-${_color}`)
+    else
+      styles.push('--a-title-color: white; --a-subtitle-color: white')
   }
   else {
     const typographyColor = _color === null ? 'var(--a-base-color)' : _color
 
-    styles.push(`--a-title-color: ${typographyColor}; --a-subtitle-color: ${typographyColor}`)
+    styles.push(`--a-title-color: ${typographyColor}; --a-subtitle-color: ${typographyColor};`)
     classes.push(
       _color === null
         ? '[--a-title-opacity:var(--a-text-emphasis-high-opacity)] [--a-subtitle-opacity:var(--a-text-emphasis-light-opacity)]'
@@ -26,18 +30,23 @@ const calculateColor = (_isThemeColor: boolean, _color: ColorProp | null) => {
   }
 }
 
-export const useTypographyColor = (color: MaybeComputedRef<ColorProp | null>) => {
-  const classes = ref([])
-  const styles = ref([])
+export const useTypographyColor = (color: MaybeComputedRef<ColorProp | null>, variant: MaybeComputedRef<string>) => {
+  const typographyClasses = ref<string[]>([])
+  const typographyStyles = ref<string[]>([])
 
   const _color = resolveUnref(color)
+  const _variant = resolveUnref(variant)
   const _isThemeColor = isThemeColor(_color)
 
-  // const {} = calculateColor(_isThemeColor, _color)
+  watch([() => color, () => variant], () => {
+    const calculatedColor = calculateColor(_isThemeColor, _color, _variant)
+    typographyClasses.value = calculatedColor.classes
+    typographyStyles.value = calculatedColor.styles
+  }, { immediate: true })
 
   return {
-    classes,
-    styles,
+    typographyClasses,
+    typographyStyles,
 
     // typographyClasses,
   }
