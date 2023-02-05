2215eca8a10b17ed1fcc9a962ca841fe8316ab9d
JD Solanki <jdsolanki0001@gmail.com>
2023-02-05 06:29:39 +0530

    chore: reset playground

--- docs/components/Playground.vue
+++ docs/components/Playground.vue
@@ -1,49 +1,6 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-
-const isLoading = ref(false)
 </script>
 
 <template>
-  <ABtn
-    v-for="v in ['fill', 'outline', 'light', 'text']"
-    :key="v"
-    color="success"
-    :variant="v"
-    :loading="isLoading"
-    @click="isLoading = !isLoading"
-  >
-    Button
-  </ABtn>
-  <ACard
-    title="awesome"
-    variant="light"
-    color="primary"
-    loading
-  >
-    <div class="a-card-body a-card-spacer">
-      <ABtn
-        v-for="v in ['fill', 'outline', 'light', 'text']"
-        :key="v"
-        color="success"
-        :variant="v"
-        :loading="isLoading"
-        @click="isLoading = !isLoading"
-      >
-        Button
-      </ABtn>
-      <AAlert
-        color="info"
-        variant="outline"
-        class="relative"
-      >
-        Fruitcake I love liquorice apple pie croissant.
-        <ALoader
-          loading
-          overlay
-          class="text-2xl"
-        />
-      </AAlert>
-    </div>
-  </ACard>
+  <ABtn>Button</ABtn>
 </template>
