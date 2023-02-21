a56851ac1a41089feead032444f8580d7eee8a04
jd-solanki <jdsolanki0001@gmail.com>
2023-02-21 16:04:53 +0530

    tests: fix failing tests by introducing setup file

--- packages/anu-vue/vite.config.ts
+++ packages/anu-vue/vite.config.ts
@@ -56,6 +56,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: ['./test/setup.vitest.ts'],
     transformMode: {
       web: [/.[tj]sx$/],
     },
