0be718e8aa648db7e03ff7235c047cac67c86563
JD Solanki <jdsolanki0001@gmail.com>
2023-05-10 07:48:49 +0530

    docs: added missing fonts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,6 +23,12 @@ if (isDev)
 export default defineConfig({
   title: 'Anu',
   description: 'DX focused utility based vue component library',
+  head: [
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap' }],
+  ],
   themeConfig: {
     footer: {
       message: 'Released under the MIT License.',
