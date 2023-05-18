887ef3e56f145a0d2c33806c26f533b2d79b3ccd
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-05-18 15:14:12 +0800

    ✨ feat(timeline): timeline implement

---
+++ packages/anu-vue/src/components/timeline/ATimeline.vue
@@ -0,0 +1,20 @@
+<template>
+  <div flex w-full justify-center>
+    <div class="timeline" w-80per grid>
+      <TimelineWapper>
+        <slot></slot>
+      </TimelineWapper>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent } from "@vue/runtime-core";
+import TimelineWapper from "./TimelimeWapper.vue";
+defineComponent({
+  TimelineWapper,
+});
+</script>
+
+<style>
+</style>
